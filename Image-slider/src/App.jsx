0ed0ec3c547950bfe83215d30eb41fbe8666f354
Sriramprasath P <sriramprasath.2002@srit.org>
2024-05-09 09:52:29 +0530

    task-3 ~ completed

---
+++ Image-slider/src/App.jsx
@@ -0,0 +1,10 @@
+import Task from "./components/Task"
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Task/>
+    </div>
+  )
+}
+
+export default App
